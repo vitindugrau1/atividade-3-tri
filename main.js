e845e9df7aa9a5b6bfb59e9bbad9efb38cd8a607
vitindugrau1 <142263154+vitindugrau1@users.noreply.github.com>
2024-10-02 11:54:30 -0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 //
 let estados =['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
+let f=15;
+while (f<5){
+    console.log (f);
+    f=f+1
 
+}
