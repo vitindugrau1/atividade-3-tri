5289dd7838054796ae2705370e80f8101ba737a1
vitindugrau1 <142263154+vitindugrau1@users.noreply.github.com>
2024-10-02 12:21:26 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 //
-let estados =['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
-let f=0;
-while (f<estados.length){
-    console.log (f);
-    console.log(estados[f]);
-    f=f++;
-}
+// let estados =['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
+// let f=0;
+// while (f<estados.length){
+//     console.log (f);
+//     console.log(estados[f]);
+//     f=f++;
+// }
+// for (let f = 0; f < estados.length; f++) {
+//     console.log (f);
+//     console.log(estados[f]);
+// }
