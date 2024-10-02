f600fa851bf315fa55627fee01aab73ba52dfa7f
vitindugrau1 <142263154+vitindugrau1@users.noreply.github.com>
2024-10-02 11:31:54 -0300

    ,,

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-alert('Victor H Tumps da Silva')
+//
+let estados =['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Santa Catarina','São Paulo','Sergipe','Tocantins']
+estados
